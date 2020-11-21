0aa62b755696484a3f1cd0acede5e3e3fdf7f8ae
Emma Huynh <emmahuynh@Emmas-MBP.fios-router.home>
2020-11-20 16:54:15 -0800

    attempting for a redeploy

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,7 @@ const express = require("express");
 const router = new express.Router();
 const createToken = require("../helpers/createToken");
 
-
-router.post("/login", async function(req, res, next) {
+router.post("/login", async function (req, res, next) {
   try {
     const user = await User.authenticate(req.body);
     const token = createToken(user);
